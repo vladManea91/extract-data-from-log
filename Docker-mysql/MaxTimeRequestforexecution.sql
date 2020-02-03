0b26aefeec633e2b4b93d7188f8a2c04d36066b0
Vlad Manea <vlad.manea@pionnix.ro>
2020-02-03 21:22:32 +0200

    3rd exercise query

--- Docker-mysql/MaxTimeRequestforexecution.sql
+++ Docker-mysql/MaxTimeRequestforexecution.sql
@@ -4,19 +4,23 @@ ALTER TABLE test_qa MODIFY Datetime TIMESTAMP;
 
 
 
-select DISTINCT tq.request, MIN(TIMEDIFF(tq.Datetime,tqi.Datetime)) as diff_time from test_qa tq
-inner join test_qa tqi on (tq.request_id = tqi.request_id and tqi.request_type = 'exit')
-where tq.request_type = 'entry' GROUP BY tq.request;
+select DISTINCT tq.request, MAX(TIMEDIFF(tq.Datetime,tqi.Datetime)) as diff_time from test_qa tq
+inner join test_qa tqi on (tq.request_id = tqi.request_id and tqi.request_type = 'entry')
+where tq.request_type = 'exit' GROUP BY tq.request;
 
+
+mysql> select DISTINCT tq.request, MAX(TIMEDIFF(tq.Datetime,tqi.Datetime)) as diff_time from test_qa tq
+    -> inner join test_qa tqi on (tq.request_id = tqi.request_id and tqi.request_type = 'entry')
+    -> where tq.request_type = 'exit' GROUP BY tq.request;
 +---------------+-----------+
 | request       | diff_time |
 +---------------+-----------+
-| processClient | -00:05:57 |
-| getActions    | -00:09:25 |
-| checkAuth     | -00:06:57 |
-| addClient     | -00:03:00 |
-| acceptToken   | -00:07:44 |
-| getData       | -00:02:56 |
-| processAction | -00:08:05 |
+| processClient | 00:05:57  |
+| getData       | 00:02:56  |
+| getActions    | 00:09:25  |
+| processAction | 00:08:05  |
+| acceptToken   | 00:07:44  |
+| checkAuth     | 00:06:57  |
+| addClient     | 00:03:00  |
 +---------------+-----------+
-7 rows in set (0.58 sec)
+7 rows in set (0.62 sec)
